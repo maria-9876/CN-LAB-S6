66efe2473f65dc1b8db94d1c0efe6ddbe3dbcca5
Maria Joseph <mariajoseph2211@gmail.com>
2025-03-25 23:46:19 +0530

    Update stopndwait.c

--- stopndwait.c
+++ stopndwait.c
@@ -1,38 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>  // for sleep()
+#include<stdio.h>
+#include<stdlib.h> //rand,srand
+#include<time.h>
+#include<unistd.h> //for sleep()
 
-#define TIMEOUT 3  // Timeout duration in seconds
-#define TOTAL_PACKETS 5  // Number of packets to send
+#define TIMEOUT 3  // no of seconds before trans if ACK lost
+#define TOTAL_PACKETS 5  //no of packets to send
 
-int simulate_acknowledgment() {
-    // Simulate a 70% chance of successful acknowledgment
-    return rand() % 10 < 7;
+int simulate_ack()
+{
+    return rand()%10<7; //fn decides if reciever successfully ack the packet
 }
 
-int main() {
-    srand(time(0));  // Seed for random number generation
-
-    int packet = 1;
-    int ack_received;
-
-    while (packet <= TOTAL_PACKETS) {
-        printf("Sender: Sending packet %d...\n", packet);
-
-        // Simulate waiting for acknowledgment
-        sleep(1);  // Simulate transmission delay
-        ack_received = simulate_acknowledgment();
-
-        if (ack_received) {
-            printf("Receiver: ACK for packet %d received.\n\n", packet);
-            packet++;  // Move to the next packet
-        } else {
-            printf("Receiver: ACK for packet %d lost! Retransmitting...\n\n", packet);
-            sleep(TIMEOUT);  // Simulate timeout before retransmission
+int main()
+{
+    srand(time(0)); //ensures each time rand() produce different values
+    int packet=1;  //start sending packets from 1
+    int ack_recieved; //stores if ack recieved
+    
+    while(packet<= TOTAL_PACKETS)
+    {
+        printf("Sender:Sending packet %d....\n",packet);
+        sleep(1);  //pauses prgm for 1 sec
+        ack_recieved =simulate_ack();
+        if(ack_recieved)  //if ack_recieved=1
+        {
+            printf("Reciever:ACK for packet %d recieved.\n\n",packet);
+            packet++;
         }
+        else   //if ack_recieved=0
+        {
+            printf("Reciever:ACK for packet %d lost!Retransmitting....\n\n",packet);
+            sleep(TIMEOUT); //sender waits for TIMEOUT sec(3) before retransmittin
+            
+        }
+       
+        
     }
-
-    printf("All packets sent successfully!\n");
+     printf("All packets sent succesfully\n");
     return 0;
 }
