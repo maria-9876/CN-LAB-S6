f95326488f0fcf07cdba5ceb0b9ee9bd7c673cda
Maria Joseph <mariajoseph2211@gmail.com>
2025-03-25 23:16:15 +0530

    Create leakybucket.c

---
+++ leakybucket.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    int n,in,out,bsize,bucket=0,possible;
+    printf("Enter the no of inputs:\n");
+    scanf("%d",&n);
+    printf("Enter bucket size:");
+    scanf("%d",&bsize);
+    printf("enter outgoing rate:");
+    scanf("%d",&out);
+    while(n!=0)
+    {
+        printf("enter no of incoming packets:");
+        scanf("%d",&in);
+        if(in <= (bsize - bucket))
+        {
+            bucket+=in;
+            printf("Bucket status:%d out of %d\n",bucket,bsize);
+        }
+        else
+        {
+            printf("dropped:%d\n",in-(bsize-bucket));
+            printf("bucket status:%d out of %d\n",bucket,bsize);
+            bucket=bsize;     //bucket full
+            
+        }
+        bucket=bucket-out;
+        printf("bucket status:%d out of %d\n",bucket,bsize);
+        n--;
+    }
+}
