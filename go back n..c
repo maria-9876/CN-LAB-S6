26dcd604722047da0f59edbcaeadeb1335902aa8
Maria Joseph <mariajoseph2211@gmail.com>
2025-03-26 09:41:41 +0530

    Update go back n..c

--- go back n..c
+++ go back n..c
@@ -2,32 +2,38 @@
 #include <stdlib.h>
 #include <time.h>
 
-int tf, N, tt = 0, i = 1;  // Global variables
+int tf, N, tt = 0, i = 1;  
+//tf:total frames to send,N:window size,tt:total frames sent,i:track 1st frame in currnrt window
 
 void transmission() {
-    while (i <= tf) {
-        int z = 0;
+    while (i <= tf)  //it ensures transmission until all frames are sent and acknowledged
+    {
+        int z = 0;   //keeps track of acknowlwdged frames
         
         // Sending frames
-        for (int k = i; k < i + N && k <= tf; k++) {
-            printf("Sending Frame %d...\n", k);
+        for (int k = i; k < i + N && k <= tf; k++) //start sending from the first frame in the current window
+            {
+            printf("Sending Frame %d...\n", k); // k as it is send 
             tt++;
         }
         
         // Receiving Acknowledgment
         for (int k = i; k < i + N && k <= tf; k++) {
-            int f = rand() % 2;
-            if (!f) {
+            int f = rand() % 2;     // f=0(ACK recieved)  f=1(ACK lost)
+            if (f==0) 
+            {
                 printf("Acknowledgment for Frame %d...\n", k);
-                z++;
-            } else {
+                z++;  // ack frames are increased
+            } 
+            else
+            {
                 printf("Timeout!! Frame Number: %d Not Received\n", k);
                 printf("Retransmitting Window...\n");
                 break;
             }
         }
-        printf("\n");
-        i = i + z;
+        printf("\n");  // Adds a blank line after processing a window
+        i = i + z;   //sliding window moves forward by numbr of ACKed frames
     }
 }
 
